e3a55e3044454cfd9a65b3d30ab4adc32d98760e
Martin Herrmann <martin.herrmann@stormi.io>
2018-08-19 23:27:24 +0200

    refactoring and rudimentary dyndomain details

--- models.py
+++ models.py
@@ -1,73 +1,8 @@
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
-
-class WhizzDivaUserManager(BaseUserManager):
-    """
-    A custom user manager to deal with emails as unique identifiers for auth
-    instead of usernames. The default that's used is "UserManager"
-    """
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The email must be set')
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, null=True)
-    is_staff = models.BooleanField(
-        "staff status",
-        default=False,
-        help_text="Designates whether the user can log into this site.",
-    )
-    is_superuser = models.BooleanField(
-        "superuser status",
-        default=False,
-        help_text="Designates whether the user has superuser privileges.",
-    )
-    is_active = models.BooleanField(
-        "active",
-        default=True,
-        help_text="Designates whether this user should be treated as active."
-                  "Unselect this instead of deleting accounts.",
-    )
-
-    USERNAME_FIELD = 'email'
-    objects = WhizzDivaUserManager()
-
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return self.email
-
-    def get_short_name(self):
-        return self.email
 
 
 class AbstractModel(models.Model):
